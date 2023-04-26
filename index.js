805df230d04ef650499d60c71a6e5b162081d30a
Sergey <vov4ok2000@yandex.ru>
2023-04-26 15:30:56 +0300

    !

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '6250482526:AAE78h917mtdqTGx6IWmV1k6FtuA69_s7AU';
 const webAppUrl = 'https://ya.ru';
+// приложение с локального хоста можно запускать через ngrok
+const webAppUrl2 = 'https://aec1-46-183-179-54.ngrok-free.app';
+const LiveCall = 'https://export.inxrm.ru:44382/dev/TestPages/LiveCall-v2.html?Data=123';
+// https://export.inxrm.ru:44382/dev/TestPages/LiveCall-v2.html
+
+// сделать форму на подобии liveCall с получением идентификатора пользователя из ТГ 
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
@@ -13,7 +19,7 @@ bot.on('message', async (msg) => {
     await bot.sendMessage(chatId, 'Добрый день! Пожалуйста, заполните форму обратной связи', {
       reply_markup: {
         keyboard: [
-          [{ text: 'Оставить заявку', web_app: { url: webAppUrl } }]
+          [{ text: 'LiveCall', web_app: { url: LiveCall } }]
         ]
       }
     });
@@ -26,5 +32,4 @@ bot.on('message', async (msg) => {
       }
     });
   }
-
 });
